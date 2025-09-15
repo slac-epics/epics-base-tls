d8b68e8fc5e89fe04c24f2c6be7da2cddc735d2b
george-mcintyre <george@level-n.com>
2025-09-15 18:57:18 +0200

    Use epics types for bytes

--- modules/libcom/src/as/asTrapWrite.h
+++ modules/libcom/src/as/asTrapWrite.h
@@ -36,7 +36,7 @@ typedef struct asTrapWriteMessage {
     const char *method; /**< \brief Method of the request (ca, anonymous, or x509). */
     const char *authority; /**< \brief Authority of the request (common name of the root certificate authority). */
     const char *hostid; /**< \brief Hostid of whoever originated the request. */
-    int8_t protocol; /**< \brief the connection is TLS encapsulated, or simple TCP. */
+    epicsInt8 protocol; /**< \brief the connection is TLS encapsulated, or simple TCP. */
     /** \brief A field for use by the server.
      *
      * Any listener that uses this field must know what type of
